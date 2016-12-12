d54538d1e755edf2c80d42f8880c15781becb40b
QIAN LEI <leilei_007000@hotmail.com>
2016-12-13 00:48:29 +0800

    12/12/2016

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+//var awesomeThoughts="I am QianLei and I am AWESOME!";
+//var funThoughts=awesomeThoughts.replace("AWESOME","FUN");
+//$("#main").append(funThoughts);
+var formattedName = HTMLheaderName.replace("%data%","QianLei");
+var formattedRole = HTMLheaderRole.replace("%data%","Web Developer");
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+var skills = ["awesomeness","programming","teaching","JS"];
+$("#main").append(skills.length);
